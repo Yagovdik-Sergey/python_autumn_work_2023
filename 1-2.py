a2a0900d3280d6275f29498eb82d804d2e2403f8
Yagovdik-Sergey <yagovdik.sergey@mail.ru>
2023-09-19 08:54:19 +0300

    Task 1-2

---
+++ 1-2.py
@@ -0,0 +1,10 @@
+x = int(input())
+y = int(input())
+sum = x + y
+sub = x - y
+multi = x * y
+div = x / y
+floor_div = x // y
+list = [sum, sub, multi, div, floor_div]
+max_number = max(list)
+print(max_number)
