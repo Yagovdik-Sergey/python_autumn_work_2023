debec9179ed92f5f8977fb8f9025f61cb4895413
Yagovdik-Sergey <yagovdik.sergey@mail.ru>
2023-09-22 09:06:38 +0300

    Update Task 2-2

--- 2-2.py
+++ 2-2.py
@@ -1,8 +1,8 @@
 import random
 lst = random.sample(range(1, 50), 10)
 print(lst)
-min = lst[0]
+min_number = lst[0]
 for i in range(len(lst)):
-    if lst[i] < min:
-        min = lst[i]
-print(f'Наименьший элемент: {min}')
+    if lst[i] < min_number:
+        min_number = lst[i]
+print(f'Наименьший элемент: {min_number}')
