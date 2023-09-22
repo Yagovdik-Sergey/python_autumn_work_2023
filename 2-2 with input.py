7a7504bfe40a2f963d7b241faf90951b60c1209e
Yagovdik-Sergey <yagovdik.sergey@mail.ru>
2023-09-22 09:36:24 +0300

    Task 2-2 with input

---
+++ 2-2 with input.py
@@ -0,0 +1,22 @@
+import random
+first = int(input("Введите число начала отрезка генерации случайного списка: "))
+last = int(input('Введите число конца отрезка генерации случайного списка: '))
+if first >= last:
+    print('Первое число должно быть больше второго')
+else:
+    if last - first < 10:
+        lst = random.sample(range(first, last + 1), last - first)
+        print(lst)
+        min_number = lst[0]
+        for i in range(len(lst)):
+            if lst[i] < min_number:
+                min_number = lst[i]
+        print(f'Наименьший элемент: {min_number}')
+    else:
+        lst = random.sample(range(first, last + 1), 10)
+        print(lst)
+        min_number = lst[0]
+        for i in range(len(lst)):
+            if lst[i] < min_number:
+                min_number = lst[i]
+        print(f'Наименьший элемент: {min_number}')
