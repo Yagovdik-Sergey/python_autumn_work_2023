cd0d9af97f2415b7640dae9951aba1dd9e968717
Yagovdik-Sergey <yagovdik.sergey@mail.ru>
2023-10-18 13:29:58 +0300

    Task 13-1 update

--- 13-1.py
+++ 13-1.py
@@ -1,11 +1,14 @@
-def func(n):
-    for x in range(1, n + 1):
-        if x % 2 == 0:
-            yield -x
-        else:
-            yield x
+def func():
+    n = 1
+    while True:
+        for _ in range(1):
+            if n % 2 == 0:
+                yield -n
+            else:
+                yield n
+        n += 1
 
 
-g = func(23)
-for i in g:
-    print(i, end=' ')
+g = func()
+for i in range(20):
+    print(next(g), end=' ')
