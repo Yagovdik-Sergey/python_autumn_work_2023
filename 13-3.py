7ca3df4f84aa8fc75d0de0ebd85591976abd038a
Yagovdik-Sergey <yagovdik.sergey@mail.ru>
2023-10-18 14:00:24 +0300

    Task 13-3 update

--- 13-3.py
+++ 13-3.py
@@ -1,10 +1,15 @@
-def func(n):
-    for i in n:
-        if i % 2 == 1:
-            yield i
+def func(def_lst):
+    for n in def_lst:
+        for i in range(1):
+            if n % 2 == 1:
+                yield n
+        n += 1
 
 
-lst = [5, 6, 7, 8, 9, 10, 11, 12]
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 g = func(lst)
-for k in g:
-    print(k, end=' ')
+for i in range(len(lst)):
+    try:
+        print(next(g), end=' ')
+    except StopIteration:
+        continue
