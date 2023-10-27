efd2b4ac8aeabfb95f4cfa09fa9ad8ef435ffb8d
Yagovdik-Sergey <yagovdik.sergey@mail.ru>
2023-10-27 14:10:54 +0300

    Task 16-1

--- 16-1.py
+++ 16-1.py
@@ -1,3 +1,3 @@
 import re
 name = input()
-print(re.sub(r"([а-яА-Яa-zA-Z])[а-я,А-Я,a-z,A-Z]+\s*", r"\1", name).upper())
+print(re.sub(r"([а-яА-ЯёЁa-zA-Z])[а-я,А-Я,ё,Ё,a-z,A-Z]+\s*", r"\1", name).upper())
