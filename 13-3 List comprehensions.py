3bdca56e766aebc43cd9f0e4fad8fb278e1e808b
Yagovdik-Sergey <yagovdik.sergey@mail.ru>
2023-12-04 14:14:29 +0300

    Task 14-1

--- 13-3 List comprehensions.py
+++
@@ -1,8 +0,0 @@
-def func(n):
-    odd_numbers = [x for x in n if x % 2 != 0]
-    return odd_numbers
-
-
-lst = [5, 6, 7, 8, 9, 11, 12, 13]
-g = func(lst)
-print(g)
--- 14-1.py
+++ 14-1.py
@@ -2,6 +2,7 @@ def digit(n):
     if n < 10:
         return 1
     else:
-        return 1 + digit(n / 10)
+        return 1 + digit(n // 10)
+
 
 print(digit(int(input())))
